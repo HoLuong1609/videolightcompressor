1af0aeee272e2cde3de595d1482788f30da03574
luong.ho <luong.ho@ntq-solution.com.vn>
2020-09-24 17:16:43 +0700

    menu drawer

--- app/src/main/java/com/example/videolightcompressor/MainActivity.kt
+++ app/src/main/java/com/example/videolightcompressor/MainActivity.kt
@@ -3,20 +3,21 @@ package com.example.videolightcompressor
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.app.ShareCompat
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import androidx.navigation.NavController
-import androidx.navigation.NavDestination
-import androidx.navigation.ui.setupWithNavController
 import com.example.videolightcompressor.extensions.openStore
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
-class MainActivity : AppCompatActivity(), NavController.OnDestinationChangedListener {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
 
@@ -30,11 +31,13 @@ class MainActivity : AppCompatActivity(), NavController.OnDestinationChangedList
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_gallery
+            ), drawerLayout
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-        navController.addOnDestinationChangedListener(this)
+        navView.setNavigationItemSelectedListener(this)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -58,11 +61,57 @@ class MainActivity : AppCompatActivity(), NavController.OnDestinationChangedList
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
-    override fun onDestinationChanged(
-        controller: NavController,
-        destination: NavDestination,
-        arguments: Bundle?
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return when (item.itemId) {
+            R.id.nav_home, R.id.nav_gallery -> {
+                drawerLayout.closeDrawer(GravityCompat.START)
+                navController.navigate(item.itemId)
+                true
+            }
+            R.id.nav_rate_app -> {
+                drawerLayout.closeDrawer(GravityCompat.START)
+                openStore()
+                true
+            }
+            R.id.nav_contact_us -> {
+                contactUs()
+                true
+            }
+            R.id.nav_share_app -> {
+                shareApp()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun shareApp() {
+        val subject = getString(R.string.app_name)
+        val bodyEmail = String.format(
+            Locale.US,
+            getString(R.string.share_app_body_msg),
+            "https://play.google.com/store/apps/details?id=$packageName"
+        )
+        startShareIntent(subject, null, bodyEmail)
+    }
+
+    private fun contactUs() {
+        val subject2 = getString(R.string.subject_email, getString(R.string.app_name))
+        val addresses = arrayOf(getString(R.string.support_email))
+        startShareIntent(subject2, addresses, null)
+    }
+
+    private fun startShareIntent(
+        subject: String?,
+        addresses: Array<String>?,
+        text: String?
     ) {
-//        invalidateOptionsMenu()
+        val intentBuilder = ShareCompat.IntentBuilder.from(this)
+        intentBuilder.setType("message/rfc822")
+        if (addresses != null) intentBuilder.setEmailTo(addresses)
+        if (subject != null) intentBuilder.setSubject(subject)
+        if (text != null) intentBuilder.setText(text)
+        intentBuilder.startChooser()
     }
 }
